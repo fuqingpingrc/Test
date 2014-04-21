784de3993c34ad9ded126121f7f468c9edd26fa8
rcplatform <rcplatform@rcplatformdeMac-mini-3.local>
2014-04-21 19:02:55 +0800

    yderyyery

--- Test/TestViewController.m
+++ Test/TestViewController.m
@@ -19,10 +19,6 @@
     [super viewDidLoad];
     
     
-    UIView *newsView = [[UIView alloc]initWithFrame:CGRectMake(0, 239, 320, 36)];
-    newsView.backgroundColor = [UIColor blackColor];
-    newsView.alpha = 0.4f;
-    
     UIPageControl *pageControl = [[UIPageControl alloc] initWithFrame:CGRectMake(270, 8, 40, 20)];
     pageControl.userInteractionEnabled = YES;
     pageControl.backgroundColor = [UIColor clearColor];
@@ -46,20 +42,15 @@
     }
     
     
-    pageControl.numberOfPages = 3;
-    pageControl.currentPage = 0; //当前页码
-    [newsView addSubview:pageControl];
-    
     UILabel *newsLabel = [[UILabel alloc]initWithFrame:CGRectMake(0, 110, 260, 16)];
     newsLabel.textColor = [UIColor whiteColor];
     newsLabel.backgroundColor = [UIColor clearColor];
     
-    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(showTabbar) name:@"y346" object:nil];
-    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(hiddenTabbar) name:@"hhrth" object:nil];
+//    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(showTabbar) name:@"y346" object:nil];
+//    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(hiddenTabbar) name:@"hhrth" object:nil];
     
     newsLabel.font = [UIFont systemFontOfSize:16.0];
     newsLabel.text = @"《甜心巧克力》发布会招聘工作人员";
-    [newsView addSubview:newsLabel];
     
     
 	// Do any additional setup after loading the view, typically from a nib.
