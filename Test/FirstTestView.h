99014060704bc0c49841c38a41903626fc3efbdc
rcplatform <rcplatform@rcplatforms-Mac-mini.local>
2014-04-21 18:36:46 +0800

    test

---
+++ Test/FirstTestView.h
@@ -0,0 +1,13 @@
+//
+//  FirstTestView.h
+//  Test
+//
+//  Created by rcplatform on 14-4-21.
+//  Copyright (c) 2014年 com.rcplatformhk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstTestView : UIView
+
+@end
---
+++ Test/FirstTestView.m
@@ -0,0 +1,31 @@
+//
+//  FirstTestView.m
+//  Test
+//
+//  Created by rcplatform on 14-4-21.
+//  Copyright (c) 2014年 com.rcplatformhk. All rights reserved.
+//
+
+#import "FirstTestView.h"
+
+@implementation FirstTestView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
