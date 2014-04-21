b0007d7abfab50515c4d60fd683fa6c9c64ed551
rcplatform <rcplatform@rcplatforms-Mac-mini.local>
2014-04-21 18:40:49 +0800

    gwewet

---
+++ Test/TestFitttttttt.m
@@ -0,0 +1,31 @@
+//
+//  TestFitttttttt.m
+//  Test
+//
+//  Created by rcplatform on 14-4-21.
+//  Copyright (c) 2014年 com.rcplatformhk. All rights reserved.
+//
+
+#import "TestFitttttttt.h"
+
+@implementation TestFitttttttt
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
